f43ff3c84d60751eba44ee6176dca674f7a046b6
satumaimo1007 <89833115+satumaimo1007@users.noreply.github.com>
2021-09-30 16:13:07 +0900

    Add files via upload

---
+++ data_do.js
@@ -0,0 +1,12 @@
+// DL8 Type-D
+// Ver 1.12.71
+var do_chs = 16;
+var do_ch = ["DO01","DO02","DO03","DO04","DO05","DO06","DO07","DO08","DO09","DO10","DO11","DO12","DO13","DO14","DO15","DO16"];
+var do_name = ["DIモニタ:No.01","DIモニタ:No.02","DIモニタ:No.03","DIモニタ:No.04","DIモニタ:No.05","DIモニタ:No.06","DIモニタ:No.07","DIモニタ:No.08","DIモニタ:No.09","DIモニタ:No.10","DIモニタ:No.11","DIモニタ:No.12","DIモニタ:No.13","DIモニタ:No.14","DIモニタ:No.15","DIモニタ:No.16"];
+var do_comm = ["DO1","DO2","DO3","DO4","DO5","DO6","DO7","DO8","DO9","DO10","DO11","DO12","DO13","DO14","DO15","DO16"];
+var do_data = ["OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF"];
+var do_chno = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16];
+var do_string_on = ["ON","ON","ON","ON","ON","ON","ON","ON","ON","ON","ON","ON","ON","ON","ON","ON"];
+var do_string_off = ["OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF","OFF"];
+var do_color = ["#000000","#000000","#000000","#000000","#000000","#000000","#000000","#000000","#000000","#000000","#000000","#000000","#000000","#000000","#000000","#000000"];
+var do_enable = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
---
+++ dl_header.js
@@ -0,0 +1,11 @@
+var year = 2021;
+var mon  = 9;
+var day  = 30;
+var hour = 15;
+var min  = 18;
+var sec  = 28;
+var dl_time1 = "2021/09/30";
+var dl_time2 = "15:18:28";
+var dl_name1 = "岩田プランテック DL8";
+var dl_name2 = "岩田プランテック";
+var dl_name3 = "";
